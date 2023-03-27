9eaf435dfd0d067da3566e771e67ae618bb8f8e4
Gabriela Zambrano Rodríguez <gabrielazaro97@gmail.com>
2023-03-27 15:02:34 +0000

    Actualizacion

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let numeros = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palos = ["\u2666", "\u2665", "\u2660", "\u2663"];
+
+  function numAleatorio() {
+    let indexNumeros = Math.floor(Math.random() * numeros.length);
+    return numeros[indexNumeros];
+  }
+  function paloAleatorio() {
+    let indexPalos = Math.floor(Math.random() * palos.length);
+    return palos[indexPalos];
+  }
+
+  const cardNumber = document.querySelector(".number");
+  cardNumber.innerHTML = numAleatorio();
+  const paloRandom = document.querySelector(".top-icon");
+  let paloA = (paloRandom.innerHTML = paloAleatorio());
+  const paloB = document.querySelector(".bottom-icon");
+  paloB.innerHTML = paloA;
+  console.log(paloB);
 };
